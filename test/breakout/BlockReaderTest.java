33bb1c01a078fa18f36c4efc0d5cadaaaa6f6781
Samarth Lamba <samarth.lamba@duke.edu>
2020-09-13 16:31:21 -0400

    more google formatting

--- test/breakout/BlockReaderTest.java
+++ test/breakout/BlockReaderTest.java
@@ -1,23 +1,23 @@
 package breakout;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class BlockReaderTest {
-    @Test
-    public void readsSimpleFileTest() throws IOException, URISyntaxException {
-        final BlockReader testReader = new BlockReader("test.txt");
-        final List<Block> blocksFromReader = testReader.getBlockList();
-        Block block = blocksFromReader.get(2);
-        assertEquals("1",block.getBlockType());
-        assertEquals(1,block.getRow());
-        assertEquals(0,block.getColumn());
-        assertEquals(2,block.getNumRows());
-        assertEquals(2,block.getNumColumns());
-    }
+
+  @Test
+  public void readsSimpleFileTest() throws IOException, URISyntaxException {
+    final BlockReader testReader = new BlockReader("test.txt");
+    final List<Block> blocksFromReader = testReader.getBlockList();
+    Block block = blocksFromReader.get(2);
+    assertEquals("1", block.getBlockType());
+    assertEquals(1, block.getRow());
+    assertEquals(0, block.getColumn());
+    assertEquals(2, block.getNumRows());
+    assertEquals(2, block.getNumColumns());
+  }
 }
--- test/breakout/GameTest.java
+++ test/breakout/GameTest.java
@@ -1,131 +1,130 @@
 package breakout;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import util.DukeApplicationTest;
 import org.junit.jupiter.api.Test;
-
-import java.util.concurrent.TimeUnit;
-
-import static org.junit.jupiter.api.Assertions.*;
+import util.DukeApplicationTest;
 
 public class GameTest extends DukeApplicationTest {
-    private final Game myGame = new Game();
-    private Scene myScene;
-    private Circle gameBall;
-    private Rectangle gamePaddle;
 
-    /**
-     * Start special test version of application that does not animate on its own before each test.
-     * <p>
-     * Automatically called @BeforeEach by TestFX.
-     */
-    @Override
-    public void start(Stage stage) {
-        // create game's scene with all shapes in their initial positions and show it
-        myScene = myGame.setupScene(Game.WIDTH, Game.HEIGHT, Game.BACKGROUND);
-        stage.setScene(myScene);
-        stage.show();
+  private final Game myGame = new Game();
+  private Scene myScene;
+  private Circle gameBall;
+  private Rectangle gamePaddle;
 
-        // find individual items within game by ID (must have been set in your code using setID())
-        gameBall = lookup("#ball").query();
-        gamePaddle = lookup("#paddle").query();
-    }
+  /**
+   * Start special test version of application that does not animate on its own before each test.
+   * <p>
+   * Automatically called @BeforeEach by TestFX.
+   */
+  @Override
+  public void start(Stage stage) {
+    // create game's scene with all shapes in their initial positions and show it
+    myScene = myGame.setupScene(Game.WIDTH, Game.HEIGHT, Game.BACKGROUND);
+    stage.setScene(myScene);
+    stage.show();
 
-    @Test
-    public void testInitialPositions() {
-        assertEquals(Game.WIDTH / 2, gameBall.getCenterX());
-        assertEquals(Game.HEIGHT / 2, gameBall.getCenterY());
-        assertEquals(Game.WIDTH / 60, gameBall.getRadius());
+    // find individual items within game by ID (must have been set in your code using setID())
+    gameBall = lookup("#ball").query();
+    gamePaddle = lookup("#paddle").query();
+  }
 
-        assertEquals(Game.WIDTH / 2 - Game.WIDTH / 10, gamePaddle.getX());
-        assertEquals(Game.HEIGHT - Game.HEIGHT / 25 - Game.HEIGHT / 35, gamePaddle.getY());
-    }
+  @Test
+  public void testInitialPositions() {
+    assertEquals(Game.WIDTH / 2, gameBall.getCenterX());
+    assertEquals(Game.HEIGHT / 2, gameBall.getCenterY());
+    assertEquals(Game.WIDTH / 60, gameBall.getRadius());
 
-    @Test
-    public void testInitialBallVelocity() {
-        final double initialXPos = gameBall.getCenterX();
-        final double initialYPos = gameBall.getCenterY();
-        assertEquals(Game.WIDTH / 2, initialXPos);
-        assertEquals(Game.HEIGHT / 2, initialYPos);
+    assertEquals(Game.WIDTH / 2 - Game.WIDTH / 10, gamePaddle.getX());
+    assertEquals(Game.HEIGHT - Game.HEIGHT / 25 - Game.HEIGHT / 35, gamePaddle.getY());
+  }
 
-        myGame.step(1.00);
+  @Test
+  public void testInitialBallVelocity() {
+    final double initialXPos = gameBall.getCenterX();
+    final double initialYPos = gameBall.getCenterY();
+    assertEquals(Game.WIDTH / 2, initialXPos);
+    assertEquals(Game.HEIGHT / 2, initialYPos);
 
-        assertEquals(initialXPos - 120, gameBall.getCenterX(), .1);
-        assertEquals(initialYPos - 120, gameBall.getCenterY(), .1);
-    }
+    myGame.step(1.00);
 
-    @Test
-    public void testPaddleMoveOnKeyPress() {
-        final double initialXPos = gamePaddle.getX();
-        final double initialYPos = gamePaddle.getY();
-        assertEquals(Game.WIDTH / 2 - Game.WIDTH / 10, initialXPos);
-        assertEquals(Game.HEIGHT - Game.HEIGHT / 25 - Game.HEIGHT / 35, initialYPos);
+    assertEquals(initialXPos - 120, gameBall.getCenterX(), .1);
+    assertEquals(initialYPos - 120, gameBall.getCenterY(), .1);
+  }
 
-        press(myScene,KeyCode.RIGHT);
+  @Test
+  public void testPaddleMoveOnKeyPress() {
+    final double initialXPos = gamePaddle.getX();
+    final double initialYPos = gamePaddle.getY();
+    assertEquals(Game.WIDTH / 2 - Game.WIDTH / 10, initialXPos);
+    assertEquals(Game.HEIGHT - Game.HEIGHT / 25 - Game.HEIGHT / 35, initialYPos);
 
-        assertEquals(initialXPos + 5, gamePaddle.getX());
-        assertEquals(initialYPos, gamePaddle.getY());
+    press(myScene, KeyCode.RIGHT);
 
-        press(myScene,KeyCode.LEFT);
+    assertEquals(initialXPos + 5, gamePaddle.getX());
+    assertEquals(initialYPos, gamePaddle.getY());
 
-        assertEquals(initialXPos, gamePaddle.getX());
-        assertEquals(initialYPos, gamePaddle.getY());
+    press(myScene, KeyCode.LEFT);
 
-        press(myScene,KeyCode.LEFT);
+    assertEquals(initialXPos, gamePaddle.getX());
+    assertEquals(initialYPos, gamePaddle.getY());
 
-        assertEquals(initialXPos-5, gamePaddle.getX());
-        assertEquals(initialYPos, gamePaddle.getY());
-    }
+    press(myScene, KeyCode.LEFT);
 
-    @Test
-    public void testBallBouncesOnPaddleCollision() {
-        gamePaddle.setY(Game.HEIGHT / 2);
-        gamePaddle.setX(Game.HEIGHT / 2);
-        final double initialXPos = gamePaddle.getX() + 120;
-        final double initialYPos = gamePaddle.getY() + 120;
-        gameBall.setCenterX(initialXPos);
-        gameBall.setCenterY(initialYPos);
+    assertEquals(initialXPos - 5, gamePaddle.getX());
+    assertEquals(initialYPos, gamePaddle.getY());
+  }
 
-        myGame.step(1.00);
-        myGame.step(1.00);
+  @Test
+  public void testBallBouncesOnPaddleCollision() {
+    gamePaddle.setY(Game.HEIGHT / 2);
+    gamePaddle.setX(Game.HEIGHT / 2);
+    final double initialXPos = gamePaddle.getX() + 120;
+    final double initialYPos = gamePaddle.getY() + 120;
+    gameBall.setCenterX(initialXPos);
+    gameBall.setCenterY(initialYPos);
 
-        assertEquals(initialXPos - 240, gameBall.getCenterX());
-        assertEquals(initialYPos, gameBall.getCenterY());
-    }
+    myGame.step(1.00);
+    myGame.step(1.00);
 
-    @Test
-    public void testBallBounceOnCorner() {
-        gameBall.setCenterX(119);
-        gameBall.setCenterY(119);
+    assertEquals(initialXPos - 240, gameBall.getCenterX());
+    assertEquals(initialYPos, gameBall.getCenterY());
+  }
 
-        myGame.step(1.00);
-        myGame.step(1.00);
+  @Test
+  public void testBallBounceOnCorner() {
+    gameBall.setCenterX(119);
+    gameBall.setCenterY(119);
 
-        assertEquals(119,gameBall.getCenterX());
-        assertEquals(119,gameBall.getCenterY());
-    }
+    myGame.step(1.00);
+    myGame.step(1.00);
 
-    @Test
-    public void testSKeySpeedUpPaddle() {
-        final double initialXPos = gamePaddle.getX();
-        final double initialYPos = gamePaddle.getY();
+    assertEquals(119, gameBall.getCenterX());
+    assertEquals(119, gameBall.getCenterY());
+  }
 
-        press(myScene,KeyCode.RIGHT);
+  @Test
+  public void testSKeySpeedUpPaddle() {
+    final double initialXPos = gamePaddle.getX();
+    final double initialYPos = gamePaddle.getY();
 
-        assertEquals(initialXPos+5, gamePaddle.getX());
-        assertEquals(initialYPos,gamePaddle.getY());
+    press(myScene, KeyCode.RIGHT);
 
-        gamePaddle.setX(initialXPos);
-        gamePaddle.setY(initialYPos);
+    assertEquals(initialXPos + 5, gamePaddle.getX());
+    assertEquals(initialYPos, gamePaddle.getY());
 
-        press(myScene,KeyCode.S);
-        press(myScene,KeyCode.RIGHT);
+    gamePaddle.setX(initialXPos);
+    gamePaddle.setY(initialYPos);
 
-        assertEquals(initialXPos+10,gamePaddle.getX());
-        assertEquals(initialYPos,gamePaddle.getY());
-    }
+    press(myScene, KeyCode.S);
+    press(myScene, KeyCode.RIGHT);
+
+    assertEquals(initialXPos + 10, gamePaddle.getX());
+    assertEquals(initialYPos, gamePaddle.getY());
+  }
 }
